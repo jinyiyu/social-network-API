a164ef50f4e483f7c28114db56191e39be5e9822
Jinyi Yu <yujinyiicxk@gmail.com>
2022-08-17 17:39:13 +0100

    users controller done with postman collections

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,34 @@
-const getAllUsers = () => {};
-const getUserById = () => {};
-const createUser = () => {};
-const updateUser = () => {};
-const deleteUser = () => {};
-const friends = () => {};
+const getAllUsers = (req, res) => {
+  try {
+    return res.json("getAllUsers");
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all Users | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
+};
+
+const getUserById = (req, res) => {
+  try {
+    return res.json("getUserById");
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get User | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
+};
+
+const createUser = (req, res) => {
+  return res.json("createUser");
+};
+
+const updateUser = (req, res) => {
+  return res.json("updateUser");
+};
+
+const deleteUser = (req, res) => {
+  return res.json("deleteUser");
+};
 
 module.exports = {
   getAllUsers,
@@ -11,5 +36,4 @@ module.exports = {
   createUser,
   updateUser,
   deleteUser,
-  friends,
 };
